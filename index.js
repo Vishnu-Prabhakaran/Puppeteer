26f9e27fa8176a750e63a7399a1f429659f6c584
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-01-12 22:37:48 +1100

    Creating an array of objects using 'map' and 'get'

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ async function main() {
   const html = await page.content();
   const $ = cheerio.load(html);
 
-  // jQuery | .result-title is the class name for the job listing
-  $(".result-title").each((index, element) => console.log($(element).text()));
+  // jQuery | .result-title is the class name for the job listing.
+  // $(".result-title").each((index, element) => console.log($(element).text()));
   // using href attribute to get the links
-  $(".result-title").each((index, element) => console.log($(element).attr("href")));
+  // $(".result-title").each((index, element) => console.log($(element).attr("href")));
 
+  // Using map function to store it in an array.
+  const results = $(".result-title")
+    .map((index, element) => {
+      const title = $(element).text();
+      const url = $(element).attr("href");
+      return { title, url };
+      // get function is always required when map is used for javascript.
+    })
+    .get();
+  console.log(results);
 }
 
 main();
