4c59bdbf28d3e64fd38f09fdef7e0736cd9e9929
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-01-12 22:06:22 +1100

    Getting the list title from Craigslist  with Cheerio

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   // Website link
-  await page.goto("https://www.google.com");
+  await page.goto(
+    "https://yakima.craigslist.org/d/software-qa-dba-etc/search/sof"
+  );
+  // html back from a page
+  const html = await page.content();
+  const $ = cheerio.load(html);
+
+  // jQuery | .result-title is the class name
+  $(".result-title").each((index, element) => console.log($(element).text()));
 }
 
 main();
