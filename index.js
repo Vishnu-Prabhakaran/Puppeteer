b9fc2e30212e93b08a28e14595856e7633b27bb8
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-01-12 23:06:37 +1100

    getting additional data from the same map function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
 async function main() {
+  // Headless to see the Puppetter in Chrome test in action
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
@@ -19,11 +20,19 @@ async function main() {
   // $(".result-title").each((index, element) => console.log($(element).attr("href")));
 
   // Using map function to store it in an array.
-  const results = $(".result-title")
+  const results = $(".result-info")
     .map((index, element) => {
-      const title = $(element).text();
-      const url = $(element).attr("href");
-      return { title, url };
+      // Using finc functio to find the PropTypes.element.
+      const titleElement = $(element).find(".result-title");
+      const timeElement = $(element).find(".result-date");
+      const hoodElement = $(element).find(".nearby");
+      const title = $(titleElement).text();
+      const url = $(titleElement).attr("href");
+      // javascript date object
+      const datePosted = new Date($(timeElement).text());
+      const hood = $(hoodElement).text();
+
+      return { title, url, datePosted, hood };
       // get function is always required when map is used for javascript.
     })
     .get();
